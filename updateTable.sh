7d59c0730c97b364d661aa35c7b52549e10815a7
meirhan99 <meirhanezzeldeen188@gmail.com>
2025-08-01 23:08:50 +0300

    fix-delete

--- updateTable.sh
+++ updateTable.sh
@@ -7,40 +7,16 @@ function update {
     source ./listTable.sh "call" "$tbName"
     [[ $tableExist -eq 0 ]] && echo "Table does not exist" && ./submenu.sh 2 && exit
 
-    select choice in "Update Table (Add Column)" "Update Rows" "Exit"; do
+    select choice in "Update Rows" "Exit"; do
         case $REPLY in
-            1) addColumn; break ;;
-            2) updateRowData; break ;;
-            3) ./submenu.sh 2; exit ;;
+            1) updateRowData; break ;;
+            2) ./submenu.sh 2; exit ;;
             *) echo "Invalid option";;
         esac
     done
 }
 
-function addColumn {
-    read -p "Enter New Column Name: " columnName
-    read -p "Enter Column Data Type (int/varchar/string): " columnType
-    source ./dataType.sh "check" "$columnType"
 
-    # Check if column already exists
-    if grep -qw "$columnName" databases/$currentDb/${tbName}_Schema; then
-        echo "Column already exists"
-        return
-    fi
-
-    [[ $dataTypeIsExist -eq 0 ]] && echo "Invalid data type" && return
-
-    # Append column to schema
-    sed -i "s/$/,${columnName},${columnType}/" databases/$currentDb/${tbName}_Schema
-    sed -i "/^$tbName,/s/$/,${columnName},${columnType}/" databases/$currentDb/Schema
-
-    # Update rows with placeholder value (e.g., NULL)
-    awk -F',' -v OFS=',' '{print $0, "NULL", "NULL"}' databases/$currentDb/$tbName > temp && mv temp databases/$currentDb/$tbName
-
-    echo "Column '$columnName' added to table '$tbName'."
-    ./submenu.sh 2
-    exit
-}
 
 function updateRowData {
     read -p "Enter Column Name to Update: " columnName
